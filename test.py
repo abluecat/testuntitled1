a6225f2699079de1e0e5841d73bec80e428d74d8
abluecat <abluecat@outlook.com>
2019-11-07 17:33:51 +0800

    修改

--- test.py
+++ test.py
@@ -182,7 +182,8 @@ now()
 #%%
 
 int('123')
-int('123', base=16)
+i = int('123', base=16)
+print(i)
 print(bin(123))
 import functools
 int2 = functools.partial(int, base=2)
@@ -205,8 +206,101 @@ def fun():
 
 
 fun()
-l=sys.argv
+l = sys.argv
 for i in l:
     print(i)
 
+
 #%%
+class Student():
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        print('%s:%s' % (self.name, self.score))
+
+
+stu1 = Student('xiaoming', 90)
+stu2 = Student('xiaohong', 95)
+stu1.print_score()
+stu2.print_score()
+
+
+# %%
+class Student():
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+
+    def print_score(self):
+        print('%s:%s' % (self.__name, self.__score))
+
+
+stu1 = Student('xiaoming', 90)
+stu1._Student__score = 95
+stu1.print_score()
+print(stu1._Student__name)
+print(hasattr(stu1,'name'))
+getattr(stu1,'name')
+
+# %%
+class Animal():
+    def run(self):
+        print('Animal is running...')
+
+
+class Dog(Animal):
+    def run(self):
+        print('Dog is running...')
+
+    def fun1(self):
+        print('this is a dog...')
+
+
+animal1 = Animal()
+animal2 = Dog()
+animal1.run()
+animal2.run()
+animal2.fun1()
+
+print(type(animal1),'***')
+print(isinstance(animal1, Animal))
+print(isinstance(animal2, Animal))
+print(dir(Animal))
+print(hasattr(animal1,'name'))
+print(hasattr(animal1,'run'))
+print(hasattr(stu1,'name'))
+# getattr(stu1,'name')
+# %%
+class Student():
+    count=0
+    def __init__(self,name):
+        self.name=name
+        Student.count+=1
+stu1=Student('xiaoming')
+stu2=Student('xiaohong')
+print(stu1.count)
+# %%
+class Student():
+    __slots__=('name','age')
+stu1=Student()
+stu1.name='xiaoming'
+print(stu1.name)
+# print(stu1.age)
+# stu1.score=90
+
+# %%
+class Student():
+    @property
+    def score(self):
+        return self._score
+    @score.setter
+    def score(self,age):
+        if age<100 and age>0:
+            self._score=age
+stu1=Student()
+stu1.score=90
+print(stu1.score)
+
+# %%
